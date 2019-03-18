689458cc9a84c4e71777690a209f20ef40d3ac2b
anton_vakhrushin <avvakhrushin@gmail.com>
2019-03-18 14:50:20 +0300

    [Trip-3] NgRx

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -7,6 +7,17 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../../environments/environment';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {EffectsModule} from '@ngrx/effects';
+import {appEffects} from '../../store/effects/index';
+import {StoreModule} from '@ngrx/store';
+import {appReducers} from '../../store/reducers';
+
+const storeDevTools = [];
+
+if (!environment.production) {
+  storeDevTools.push(StoreDevtoolsModule.instrument());
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +28,10 @@ import {MatButtonModule} from '@angular/material';
     BrowserAnimationsModule,
     MatButtonModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot(appEffects),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ...storeDevTools
   ],
   providers: [],
   bootstrap: [AppComponent]
