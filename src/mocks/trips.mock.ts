9a85975269b13a2e072c281420c4e6f0eac0cefa
anton_vakhrushin <avvakhrushin@gmail.com>
2019-03-24 06:38:50 +0300

    [Trip-12] complete trip card

--- src/mocks/trips.mock.ts
+++ src/mocks/trips.mock.ts
@@ -4,7 +4,35 @@ export function getTrip():  TripModel {
   return {
     id: 123,
     userId: 123,
-    title: 'Лучший трип в грузию',
+    startDate: 1553391844305,
+    endDate: 1573391890901,
+    path: [
+      {
+        id: 1,
+        country: 'Russia',
+        city: 'Moscow'
+      },
+      {
+        id: 2,
+        country: 'Cyprus'
+      },
+      {
+        id: 3,
+        country: 'Georgia',
+        city: 'Tbilisi'
+      },
+      {
+        id: 4,
+        country: 'Georgia',
+        city: 'Batumi'
+      },
+      {
+        id: 1,
+        country: 'Russia',
+        city: 'Moscow'
+      }
+    ],
+    title: 'Best trip in Georgia!',
     photoUrl: 'https://images.unsplash.com/photo-1504964148034-86ded740d1e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
   };
 }
--- src/shared/helpers/image.helper.ts
+++ src/shared/helpers/image.helper.ts
@@ -1,9 +1,22 @@
-export function transformImageToContainer(selector: string, containerHeight: number) {
-  const images = document.querySelectorAll(`${selector}`) as any;
+export function transformImageToContainer(containerSelector: string, imageSelector: string): boolean[] {
+  const containerHeight = (document.querySelector(containerSelector) as HTMLElement).offsetHeight;
+  const containerWidth = (document.querySelector(containerSelector) as HTMLElement).offsetWidth;
+  const images = [].slice.call(document.querySelectorAll(imageSelector));
+  const imageLoads = new Array(images.length);
 
-  images.forEach(image => {
-    const height = image.height;
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
 
-    image.style.transform = `translateY(-${(height - containerHeight) / 2}px)`;
-  });
+    image.onload = () => setTimeout(() => {
+      const imageHeight = image.height;
+      const imageWidth = image.width;
+      const newImageHeight = imageHeight * (containerWidth / imageWidth);
+
+      imageLoads[i] = true;
+
+      image.style.transform = `translateY(-${(newImageHeight - containerHeight) / 2}px)`;
+    }, 500);
+  }
+
+  return imageLoads;
 }
